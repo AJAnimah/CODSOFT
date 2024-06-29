593c7a7f5414b221c8124b457203c348179a0c60
Your Name <you@example.com>
2024-06-29 18:21:23 +0100

    Initial commit with three tasks: Landing Page, Portfolio Website, and Calculator

---
+++ Task2Portfolio Website/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings:["a Frontend Web Developer" , "a Graphic Designer" , "an Administrative Professional"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
+
